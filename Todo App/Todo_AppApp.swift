0047d50a9fe5345a716ccd8e1350a96eb0434b7d
dominikdeimel <info@dominikdeimel.com>
2023-12-19 16:40:47 +0100

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Dominik Deimel on 19.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
